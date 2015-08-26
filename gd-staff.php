dac62a67c84d64d299389fccb8e7e5cd245a750c
Bradford Knowlton <brad@ucanbonline.net>
2015-08-26 12:20:50 -0600

    initial commit

---
+++ gd-staff.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: GD Staff
+Plugin URI: http://sussexschool.org/
+Description: Used by millions, to make WP better.
+Version: 1.0.1
+Author: Grow Design
+Author URI: http://www.growdesign.net/
+License: GPLv2 or later
+GitHub Plugin URI: https://github.com/GrowDesign/GD-staff
+*/
+
