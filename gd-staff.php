ca9995ca31fa6bedaf508e84a1421101d146855f
Bradford Knowlton <brad@ucanbonline.net>
2015-08-26 12:32:45 -0600

    Added custom post type

--- gd-staff.php
+++ gd-staff.php
@@ -4,10 +4,52 @@
 Plugin Name: GD Staff
 Plugin URI: http://sussexschool.org/
 Description: Used by millions, to make WP better.
-Version: 1.0.2
+Version: 1.0.3
 Author: Grow Design
 Author URI: http://www.growdesign.net/
 License: GPLv2 or later
 GitHub Plugin URI: https://github.com/GrowDesign/GD-staff
 */
 
+add_action( 'init', 'register_cpt_staff' );
+
+function register_cpt_staff() {
+
+    $labels = array( 
+        'name' => _x( 'Staff', 'staff' ),
+        'singular_name' => _x( 'Staff', 'staff' ),
+        'add_new' => _x( 'Add New', 'staff' ),
+        'add_new_item' => _x( 'Add New Staff', 'staff' ),
+        'edit_item' => _x( 'Edit Staff', 'staff' ),
+        'new_item' => _x( 'New Staff', 'staff' ),
+        'view_item' => _x( 'View Staff', 'staff' ),
+        'search_items' => _x( 'Search Staff', 'staff' ),
+        'not_found' => _x( 'No staff found', 'staff' ),
+        'not_found_in_trash' => _x( 'No staff found in Trash', 'staff' ),
+        'parent_item_colon' => _x( 'Parent Staff:', 'staff' ),
+        'menu_name' => _x( 'Staff', 'staff' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => true,
+        
+        'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
+        
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        
+        
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'staff', $args );
+}
